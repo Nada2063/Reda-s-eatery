2e05a4d8c069442a0651b39c4ee4ac4f5d25ea23
Nada2063 <120319804+Nada2063@users.noreply.github.com>
2023-10-20 17:24:22 +0200

    Home page need changes

---
+++ home.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="homecss.css">
+    <script src="https://kit.fontawesome.com/f4bd0b4361.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+    <div class="container">
+        <h1>REDA'S EATERY</h1>
+
+     <!-- menu and cancel for responsive -->
+        <i class="fa-solid fa-bars" id="fx" onclick="showmenu()"></i>
+        <div class="navbar" id="naxx">
+
+            <i class="fa-sharp fa-solid fa-xmark" id="fx" onclick="hidemenu()"></i>
+
+            <ul>
+
+                <li>
+                    <input class="search" type="text" placeholder="Search Food">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                  </li>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li>
+                    <i class="fa-solid fa-cart-shopping" onclick="location.href = 'check_out.html';"></i>
+                    <a href=""></a>
+               
+                </li>
+                <li><a  class="sign" href="user_profile.html">Sign in</a></li>
+                <li><a class="sign" href="register.html">Sign up</a></li>
+            </ul>
+
+        </div>
+
+
+
+    </div>
+
+    <div class="content">
+        <h3>Welcome To Reda's eatery</h3>
+        <h1>Returant & Cafe</h1>
+      <button onclick="location.href = 'index.html';">Pick your table</button>
+    </div>
+
+   <section class="alifood">
+        <h1>How it Works</h1>
+       <div class="row">
+        <div class="foodcol">
+            <h1>1</h1>
+            <h3>You place your order</h3><br>
+            <p>You choose your order type and area to see all the kitchens that operate in that area and their menus and place your order with only three total clicks.</p>
+        </div>
+
+        <div class="foodcol">
+            <h1>2</h1>
+            <h3>Homechef starts cooking</h3><br>
+            <p> Mumms’ homechefs receive your order instantly and start cooking you delicious meals, all prepared in their pre-approved kitchens. All the kitchens go through a serious process of hygiene and safety checks</p>
+        </div>
+
+        <div class="foodcol">
+            <h1>3</h1>
+            <h3>Order delivery</h3><br>
+            <p>One of Mumm’s delivery guys will pick up your order from the homechef’s kitchen and deliver it to your doorstep</p>
+        </div>
+
+        
+
+       </div>
+   </section>
+
+
+
+   <section class="dishes">
+    <h1>Dishes Types</h1>
+    <div class="row">
+        <div class="foodcol2">
+            <img src="photos/veget.jpg" alt="">
+            <div class="layer"><h3>
+                Single Dishes
+            </h3></div>
+        </div>
+
+        <div class="foodcol2">
+            <img src="photos/zzz.jpeg" alt="">
+            <div class="layer"><h3>
+                Frozen Dishes
+            </h3></div>
+        </div>
+
+        <div class="foodcol2">
+            <img src="photos/ooo.jpg" alt="">
+            <div class="layer"><h3>
+                Family Dishes
+            </h3></div>
+        </div>
+    </div>
+
+
+   </section>
+
+
+
+   <section class="problems">
+    <h1>Problems we Solve</h1>
+    <p>Mumm is an online platform connecting talented homebased cooks with hungry professionals to give them access to wholesome fresh homemade food, from the homechef’s kitchen.</p>
+
+<div class="row">
+    <div class="foodcol3">
+        <img src="photos/ex1.jpeg" alt="">
+        <h3>Is eating healthy really more expensive?</h3>
+        <p>It's a common belief that eating healthy is too expensive. In 2013, the Harvard School of Public Health analyzed 27 studies and found that it cost an average of $1.50 more per day to eat healthy. </p>
+    </div>
+
+    <div class="foodcol3">
+        <img src="photos/ppp.jpeg" alt="">
+        <h3>It takes too much time to prepare homemade food?</h3>
+        <p>It's a common belief that eating healthy is too expensive. In 2013, the Harvard School of Public Health analyzed 27 studies and found that it cost an average of $1.50 more per day to eat healthy. </p>
+    </div>
+
+
+
+    <div class="foodcol3">
+        <img src="photos/aaa3.jpeg" alt="">
+        <h3>Junk food is really not healthy?</h3>
+        <p>It's a common belief that eating healthy is too expensive. In 2013, the Harvard School of Public Health analyzed 27 studies and found that it cost an average of $1.50 more per day to eat healthy. </p>
+    </div>
+
+
+</div>
+</section>
+
+
+<section class="rates">
+    <h1>What Our Customers Says</h1>
+
+
+    <div class="row">
+        <div class="alirates">
+            <img src="photos/model1.jpeg" alt="">
+            <div>
+                <p>
+                    ting talented homebased cooks with hungry professting talented homebased cooks with hungry professting talented homebased cooks with hungry professting talented homebased cooks with hungry professting talented homebased cooks with hungry profess
+                </p>
+                <h3>Eslam Ahmed</h3>
+            </div>
+        </div>
+
+
+        <div class="alirates">
+            <img src="photos/model2.jpeg" alt="">
+            <div>
+                <p>
+                    ting talented homebased cooks with hungry professting talented homebased cooks with hungry professting talented homebased cooks with hungry professting talented homebased cooks with hungry professting talented homebased cooks with hungry profess
+                </p>
+                <h3>Saher Amr</h3>
+            </div>
+        </div>
+
+
+
+    </div>
+
+</section>
+
+
+   
+
+
+
+<script>
+var navv=document.getElementById("naxx")
+function showmenu(){
+    naxx.style.right="0"
+}
+function hidemenu(){
+    naxx.style.right="-200px"
+}
+</script>
+
+eserved by &copy;conceptial 2020</p>
+</div>
+</footer> 
+
+
+<footer>
+    <div class="main-content">
+      <div class="left box">
+        <h2 style="color: white;">About us</h2>
+        <div class="content">
+          <p>Reda's eatery ia a resturant which services diffrent  kinds of food and provide a good vibe for customesrs</p>
+          <div class="social">
+            <a href="https://www.facebook.com/RedaEatery"><span class="fab fa-facebook-f"></span></a>
+            <a href="#"><span class="fab fa-twitter"></span></a>
+            <a href="https://instagram.com/redaeatery?igshid=NTc4MTIwNjQ2YQ=="><span class="fab fa-instagram"></span></a>
+            <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+          </div>
+        </div>
+      </div>
+      <div class="center box">
+        <h2 style="color: white;">Address</h2>
+        <div class="content">
+          <div class="place">
+            <span class="fas fa-map-marker-alt"></span>
+            <span class="text">Fifth district,Obour City</span>
+          </div>
+          <div class="phone">
+            <span class="fas fa-phone-alt"></span>
+            <span class="text">01025999745</span>
+          </div>
+          <div class="email">
+            <span class="fas fa-envelope"></span>
+            <span class="text">abc@example.com</span>
+          </div>
+        </div>
+      </div>
+      <div class="right box">
+        <h2 style="color: white;">Contact us</h2>
+        <div class="content">
+          <form action="#">
+            <div class="email">
+              <div class="text">Email *</div>
+              <input type="email" required style="width: 50%;">
+            </div>
+            <div class="msg">
+              <div class="text">Message *</div>
+              <textarea rows="2" cols="10" required ></textarea>
+              
+            </div>
+           
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+     
+      
+        <span style="text-align: center;" class="far fa-copyright"></span><span> 2020 All rights reserved.</span>
+      
+    </div>
+  </footer>
+
+
+
+</body>
+</html>
